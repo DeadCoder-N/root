819df25d57c31906ea69d0601313a75137c4d815
Nitesh-droid <niteshsawardekar972@gmail.com>
2025-12-02 05:26:57 +0530

    2 Dec 2025

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+
+
+
 export default defineConfig({
   plugins: [react()],
+  base: "/root/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },

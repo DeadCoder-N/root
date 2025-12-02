4536335d4b186ed7bd3a1ce9a6b7191aa9ff1245
DeadCoder-N <niteshsawardekar972@gmail.com>
2025-12-02 06:57:50 +0530

    2 Dec 2025

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/root/",
+  base: "/root",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },

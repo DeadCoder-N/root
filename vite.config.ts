d9bf4efd387e58d92bee3089571768b5f921a7e6
Nitesh Sawardekar <niteshsawardekar972@gmail.com>
2026-03-06 07:24:24 +0530

    Fix robots.txt for both deployments

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: process.env.VERCEL ? "/" : "/root",
+  base: process.env.VERCEL || mode === 'development' ? "/" : "/root",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
